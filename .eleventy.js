b377a3da143a3293c3215343409fbc4612857cd5
nailahgucon <nailahgucon@gmail.com>
2021-05-23 21:37:25 +0800

    update: add PrismJS to aid in syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addWatchTarget("./src/css/");
+    eleventyConfig.addPlugin(syntaxHighlight);
     return {
         dir: {
             input: "src",
